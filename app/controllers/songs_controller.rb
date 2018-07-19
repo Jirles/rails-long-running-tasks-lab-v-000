5b64fc2a3da2c846dfce3946fe487a70e300c6d5
Emily Jirles <emily.jirles@gmail.com>
2018-07-19 02:02:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
 
   def upload 
     binding.pry
-    CSV.foreach(params[:song][:csv].path, headers: true) do |song|
+    CSV.foreach(params[:file].path, headers: true) do |song|
       Song.create(title: song[0], artist_id: Artist.find_by(name: song[1]))
     end 
   end 
